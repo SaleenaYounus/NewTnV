91471ebe6f6238da9dddf65731c5f00c4516d19b
SaleenaYounus <saleenans1@gmail.com>
2022-01-09 16:06:07 +0530

    tinder with location

--- app/src/main/java/com/example/tinderv1/RegistrationActivity.java
+++ app/src/main/java/com/example/tinderv1/RegistrationActivity.java
@@ -1,16 +1,20 @@
 package com.example.tinderv1;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -19,28 +23,106 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
 
 import java.util.HashMap;
 import java.util.Map;
 
+//above packages are  are related to location
+
 public class RegistrationActivity extends AppCompatActivity {
 
-    private Button mRegister;
+
+    private Button mRegister,mLocation;
     private EditText mEmail, mPassword, mName;
+    private TextView textView;
 
     private RadioGroup mRadioGroup;
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
 
+    Button btnShowLocation;
+    private static final int REQUEST_CODE_PERMISSION = 2;
+    String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
+
+    // GPSTracker class
+    GPSTracker gps;
+    private DocumentReference db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        try {
+            if (ActivityCompat.checkSelfPermission(this, mPermission)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this, new String[]{mPermission},
+                        REQUEST_CODE_PERMISSION);
+                // If any permission above not allowed by user, this condition will
+                // execute every time, else your else part will work
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        btnShowLocation = (Button) findViewById(R.id.userlocation);
+        textView = (TextView) findViewById(R.id.textView);
+
+        // show location button click event
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // create class object
+                gps = new GPSTracker(RegistrationActivity.this);
+
+                // check if GPS enabled
+                if(gps.canGetLocation()){
+
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+                 /*   // Compute the GeoHash for a lat/lng point
+                    double lat = latitude ;
+                    double lng = longitude;
+                    String hash = GeoFireUtils.getGeoHashForLocation(new GeoLocation(lat, lng));
+
+// Add the hash and the lat/lng to the document. We will use the hash
+// for queries and the lat/lng for distance comparisons.
+                    Map<String, Object> updates = new HashMap<>();
+                    updates.put("geohash", hash);
+                    updates.put("lat", lat);
+                    updates.put("lng", lng);
+
+                    DocumentReference londonRef = db.collection("cities").document("LON");
+                    londonRef.update(updates)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    // ...
+                                }
+                            });*/
+                    textView.append("\n" +"\nLat:" + latitude +"\nLong:" + longitude);
+                    // \n is for new line
+                   // Toast.makeText(getApplicationContext(), "Your Location is - \nLat: "
+                        //    + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+                }else{
+                    // can't get location
+                    // GPS or Network is not enabled
+                    // Ask user to enable GPS/network in settings
+                    gps.showSettingsAlert();
+                }
+
+            }
+        });
+
+
         mAuth = FirebaseAuth.getInstance();
         firebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
-            @Override
+
+
+         @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 if (user !=null){
@@ -54,12 +136,16 @@ public class RegistrationActivity extends AppCompatActivity {
 
 
         mRegister = (Button) findViewById(R.id.register);
+        mLocation = (Button) findViewById(R.id.userlocation);
 
         mEmail = (EditText) findViewById(R.id.email);
         mPassword = (EditText) findViewById(R.id.password);
         mName = (EditText) findViewById(R.id.name);
+       // mCoordinates = (EditText) findViewById(R.id.coordinates);
 
         mRadioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+
+
 
         mRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,6 +171,8 @@ public class RegistrationActivity extends AppCompatActivity {
                             DatabaseReference currentUserDb = FirebaseDatabase.getInstance().getReference().child("Users").child(userId);
                             Map userInfo = new HashMap<>();
                             userInfo.put("name", name);
+                            userInfo.put("Latitude",gps.getLatitude());
+                            userInfo.put("Longitude",gps.getLongitude());
                             userInfo.put("sex", radioButton.getText().toString());
                             userInfo.put("profileImageUrl", "default");
                             currentUserDb.updateChildren(userInfo);
